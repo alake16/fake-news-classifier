a4158f30824d983e34ec8e9e63395976b8d228ff
alake16 <aclake@umich.edu>
2019-11-26 16:02:49 -0600

    Preprocessing and attempting to build model

---
+++ classifier.py
@@ -0,0 +1,17 @@
+from preprocessing import preprocessing
+from keras.models import Sequential
+from keras.layers import Dense
+
+X_train, X_test, y_train, y_test, X_train_fit, vectorizer = preprocessing()
+ 
+model = Sequential()
+ 
+model.add(Dense(units=500, activation='relu', input_dim=len(vectorizer.get_feature_names())))
+model.add(Dense(units=1, activation='sigmoid'))
+ 
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+model.fit(X_train_fit, y_train, epochs=2, batch_size=128, verbose=1, validation_split=0.33)
+scores = model.evaluate(vectorizer.transform(X_test), y_test, verbose=1)
+print("Accuracy:", scores[1]) 
---
+++ preprocessing.py
@@ -0,0 +1,41 @@
+import re
+import nltk
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+
+def fetch_dataset():
+    df = pd.read_csv("./data/news_dataset.csv")
+    df = df.drop(columns=['Unnamed: 0'])
+    # drop rows with missing values
+    df = df.dropna()
+    df['title'] = df['title'].apply(clean_text)
+    df['content'] = df['content'].apply(clean_text)
+    print(df.info())
+    # Split Train and Test sets
+    X_train, X_test, y_train, y_test = train_test_split(df["content"], df["label"], test_size = .15, random_state=0)
+    return X_train, X_test, y_train, y_test
+
+def clean_text(text):
+    # Strip HTML tags
+    text = re.sub('<[^<]+?>', ' ', text)
+    # Strip escaped quotes
+    text = text.replace('\\"', '')
+    # Strip quotes
+    text = text.replace('"', '')
+    return text
+
+def vectorizer(X_train):
+    vectorizer = CountVectorizer(binary=True, stop_words=stopwords.words('english'), 
+                             lowercase=True, max_features=5000)
+    X_train_fit = vectorizer.fit_transform(X_train)
+    return X_train_fit, vectorizer
+
+def preprocessing():
+    X_train, X_test, y_train, y_test = fetch_dataset()
+    X_train_fit, vectorizer_var = vectorizer(X_train)
+    return X_train, X_test, y_train, y_test, X_train_fit, vectorizer_var
+
+# X_train, X_test, y_train, y_test = fetch_dataset()
+# X_train_fit, vectorizer = vectorizer(X_train)
